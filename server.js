466c704b554cda821c053191fedf2e1cd48fd8d5
Kyle Mettling <kylem@kenblanchard.com>
2021-04-06 16:26:49 -0700

    Added dynamic date functionality

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
 const PORT = 5000;
+
+// Set date and month
+let today = new Date()
+const date = {day: today.getDate(), month: today.getMonth() + 1}
 // const dummyData = require("./data.json");
 require("dotenv").config({ path: `${__dirname}/config/process.env` });
 
@@ -10,8 +14,9 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const API_KEY = process.env.API_KEY;
-const API_ENDPOINT = `https://api.sportradar.us/mlb/trial/v7/en/games/2021/04/04/schedule.json?api_key=${API_KEY}`;
-
+const API_ENDPOINT = `https://api.sportradar.us/mlb/trial/v7/en/games/2021/${date.day}/${date.month}/schedule.json?api_key=${API_KEY}`;
+let gameLinks, gameData
+console.log(date);
 // dummyData manipulation
 // let games = dummyData.games;
 
@@ -22,23 +27,28 @@ const API_ENDPOINT = `https://api.sportradar.us/mlb/trial/v7/en/games/2021/04/04
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
-
 // @route Game Schedule page
 app.get("/getGames", async (req, res) => {
   const gameList = await fetch(API_ENDPOINT)
     .then((res) => res.json())
     .then((body) => {
       // returnGameSchedule(body);
-      res.render("games.ejs", { info: returnGameSchedule(body) });
+      getGameLinks(body)
+      // console.log(returnGameIds(body));
+      res.render("games.ejs", { info: returnGameSchedule(body), 
+        gameLinks });
     })
     .catch((err) => console.log(err));
 });
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
 
-// Get schedule games from JSON response
 function returnGameSchedule(resBody) {
   return resBody.games.map((a) => (a = `${a.away.name} @ ${a.home.name}`));
-  // res.render("games.ejs", { info: games });
-  // res.render("games.ejs", { info: games });
+}
+function getGameLinks(resBody) {
+  gameData = (resBody.games.map((a, b) => a = a))
+  console.log(gameData.length);
+  gameLinks =  resBody.games.map((a) => a = `${a.home.abbr}-${a.away.abbr}`)
+
 }
