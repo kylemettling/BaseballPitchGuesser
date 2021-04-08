3808eaab26d807cbdf690bfc5b20898e26485f41
WON-DER <wonderfilledyt@gmail.com>
2021-04-07 20:32:18 -0700

    Added rough draft of gameday.js file

---
+++ models/gameday.js
@@ -0,0 +1,15 @@
+class GamedayDetails {
+  constructor(inputData) {
+    this.gameId = inputData.id;
+    this.scheduled = Date(inputData.scheduled).split(" ").slice(1, 5).join(" ");
+    this.venue = `${inputData.venue.name} in ${inputData.venue.market}, ${inputData.venue.state}`;
+    this.home = inputData.home.name;
+    this.away = inputData.away.name;
+    this.title = `${this.away} @ ${this.home}`;
+  }
+  //   matchup() {
+  //     return `${this.away} @ ${this.home}`;
+  //   }
+}
+
+module.exports = GamedayDetails;
--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
 const PORT = 5000;
+const GamedayDetails = require("./models/gameday");
 
 // Set date and month
-let today = new Date()
-const date = {day: today.getDate(), month: today.getMonth() + 1}
-// const dummyData = require("./data.json");
+let today = new Date();
+const date = { day: today.getDate(), month: today.getMonth() + 1 };
+const dummyData = require("./data.json");
 require("dotenv").config({ path: `${__dirname}/config/process.env` });
 
 app.set("view engine", "ejs");
@@ -15,13 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const API_KEY = process.env.API_KEY;
 const API_ENDPOINT = `https://api.sportradar.us/mlb/trial/v7/en/games/2021/${date.day}/${date.month}/schedule.json?api_key=${API_KEY}`;
-let gameLinks, gameData
-console.log(date);
-// dummyData manipulation
-// let games = dummyData.games;
-
-// games = games.map((a, b) => (a = `${a.away.name} @ ${a.home.name}`));
-// console.log(games);
+let gameLinks, gameData;
+// console.log(date);
+getDummyGameLinks(dummyData);
+// console.log(gameLinks);
 
 // @route Homepage
 app.get("/", (req, res) => {
@@ -29,16 +27,35 @@ app.get("/", (req, res) => {
 });
 // @route Game Schedule page
 app.get("/getGames", async (req, res) => {
-  const gameList = await fetch(API_ENDPOINT)
-    .then((res) => res.json())
-    .then((body) => {
-      // returnGameSchedule(body);
-      getGameLinks(body)
-      // console.log(returnGameIds(body));
-      res.render("games.ejs", { info: returnGameSchedule(body), 
-        gameLinks });
-    })
-    .catch((err) => console.log(err));
+  // Live API call
+  // const gameList = await fetch(API_ENDPOINT)
+  //   .then((res) => res.json())
+  //   .then((body) => {
+  //     getGameLinks(body);
+  //     res.render("games.ejs", { info: returnGameSchedule(body), gameLinks });
+  //   })
+  //   .catch((err) => console.log(err));
+
+  // Dummy API call
+  res.render("games.ejs", {
+    info: returnDummyGameSchedule(dummyData),
+    gameLinks,
+  });
+});
+
+app.get("/:game", (req, res) => {
+  const game = req.params;
+  const [home, away] = game.game.split("-");
+  // res.json(home, away);
+  // console.log(home, away);
+  const GamePage = new GamedayDetails(
+    dummyData.games.find((a) => (a.id = game))
+  );
+  const { title, venue, scheduled } = GamePage;
+  console.log(venue, scheduled);
+  // console.log(GamePage);
+  res.render("gameDetails.ejs", { title, venue, scheduled });
+  // res.json(GamePage);
 });
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
@@ -46,9 +63,15 @@ app.listen(PORT, console.log(`Server running on port ${PORT}`));
 function returnGameSchedule(resBody) {
   return resBody.games.map((a) => (a = `${a.away.name} @ ${a.home.name}`));
 }
+function returnDummyGameSchedule(dummyData) {
+  return dummyData.games.map((a) => (a = `${a.away.name} @ ${a.home.name}`));
+}
 function getGameLinks(resBody) {
-  gameData = (resBody.games.map((a, b) => a = a))
+  gameData = resBody.games;
   console.log(gameData.length);
-  gameLinks =  resBody.games.map((a) => a = `${a.home.abbr}-${a.away.abbr}`)
-
+  gameLinks = resBody.games.map((a) => (a = `${a.home.abbr}-${a.away.abbr}`));
+}
+function getDummyGameLinks(resBody) {
+  // gameLinks = dummyData.games.map((a) => (a = `${a.home.abbr}-${a.away.abbr}`));
+  gameLinks = dummyData.games.map((a) => (a = `${a.id}`));
 }
