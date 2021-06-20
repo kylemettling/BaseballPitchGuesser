c8981c4f976c5c75c2cece16a8a18a7c86ddb5b2
WON-DER <wonderfilledyt@gmail.com>
2021-06-19 17:58:36 -0700

    eliminated unneeded code in /controllers

--- controllers/gamePage.js
+++ controllers/gamePage.js
@@ -13,12 +13,10 @@ module.exports = {
       filteredGuesses,
       guessResult;
     async function getPitchGuesses() {
-      // console.log(req.body);
       try {
         await User.findById(req.user.id)
           .lean()
           .then((body) => {
-            // console.log(body.pitchGuesses);
             guesses = body.pitchGuesses[body.pitchGuesses.length - 1];
           });
       } catch (err) {
@@ -32,51 +30,40 @@ module.exports = {
           currentPitchNumber,
           currentPitchZone,
           await guesses,
-          name,
+          username,
           matchupId
         );
       } catch (err) {
         console.log(err);
       }
-      // console.log(guessResult);
       return guessResult;
     }
     try {
       const { matchupId } = req.params;
-      // console.log(PLAYBYPLAY_ENDPOINT.replace("gameId", matchupId));
+      console.log(PLAYBYPLAY_ENDPOINT.replace("gameId", matchupId));
       getPitchGuesses();
-      // console.log(guessResult);
-      // await setTimeout(
-      //   () =>
       fetch(PLAYBYPLAY_ENDPOINT.replace("gameId", matchupId))
         .then((res) => res.json())
         .then((body) => {
           const boxscore = new BoxScore(body.game);
+          username = req.user.firstName;
           currentPitchNumber = boxscore.currentPitchNumber;
           currentPitchZone = boxscore.currentPitchZone;
-          // console.log(currentPitchNumber, currentPitchZone);
-          getResults(req.user.firstName);
 
-          // console.log(guessResult, req.user.firstName);
           return boxscore;
         })
-        .then(async (boxscore, name) =>
+        .then(async (boxscore) => {
           res.render("gameDetails.ejs", {
             box: boxscore,
-            // userGuess: guessResult,
-            userGuess: await getResults(name),
-          })
-        );
-      //   1000
-      // );
+            userGuess: await getResults(username),
+          });
+        });
     } catch (err) {
       console.log(err);
     }
   },
   postZoneChoice: async (req, res) => {
     const { matchupId } = req.params;
-    // const { pitchGuess, gameid, sequencenumber } = await req.body;
-    // const { pitchGuess, gameid } = JSON.stringify(req.body);
     const getCurrentPitch = await fetch(
       PLAYBYPLAY_ENDPOINT.replace("gameId", matchupId)
     )
@@ -85,23 +72,11 @@ module.exports = {
         const boxscore = new BoxScore(body.game);
         currentPitchNumber = boxscore.currentPitchNumber;
         currentPitchZone = boxscore.currentPitchZone;
-        // console.log(currentPitchNumber, currentPitchZone);
-        // getResults(req.user.firstName);
         return boxscore.currentPitchNumber;
       });
-    // .then(
-    //   (boxscore) =>
-    //   res.render("gameDetails.ejs", {
-    //     box: boxscore,
-    //     userGuess: guessResult,
-    //   })
-    // );
     console.log(`current pitch: ${await getCurrentPitch}`);
     try {
       const { pitchGuess, gameid } = req.body;
-      // console.log(`body: ${JSON.stringify(req.body)}`);
-      // console.log([pitchGuess, await getCurrentPitch, gameid]);
-      // const {}
       await User.findOneAndUpdate(
         { _id: req.user.id },
         {
@@ -111,7 +86,6 @@ module.exports = {
               sequencenumber: await getCurrentPitch,
               gameid,
             },
-            // pitchGuesses: req.body,
           },
         },
         { new: true }
