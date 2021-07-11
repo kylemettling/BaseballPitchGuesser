f79a0b72dc8748c37a5279f3789a9e44fb1038eb
WON-DER <wonderfilledyt@gmail.com>
2021-07-11 14:15:07 -0700

    addressing prod bug

--- public/js/gameday/main.js
+++ public/js/gameday/main.js
@@ -32,6 +32,7 @@ function submitZoneChoice(e) {
       // .then((res) => res.end())
       .then((res) => {
         console.log(res);
+        console.log(req);
         console.log(window.location.href);
         // fetch(window.location.href);
         // console.log("=== YES RELOADING ===");
